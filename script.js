a2510c76bfbb52ced047e62010d852c42b2e107a
Costa-31 <bodozolii999@gmail.com>
2024-03-14 14:46:20 +0100

    test7

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 /*window.onscroll = function () {
   scrollFunction();
 };*/
-
+/*
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -18,7 +16,7 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
+*/
 //for scroll back after refresh
 
 function saveScrollPosition() {
